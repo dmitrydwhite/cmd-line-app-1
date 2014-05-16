285ccecadb522ac84b9a1bbc22fa6f5e25fb016a
Dmitry White <white.dmitry@gmail.com>
2014-05-16 11:16:31 -0700

    Using Commander interface, program adds many numbers, subtracts one number from another, and multiplies.

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
-var toNum = function(inputString) {
-	var nowNum = inputString.parseInt;
-	return nowNum;
-};
+
 
 module.exports.add = function(inputArray) {
 	var newSum = 0;
 	inputArray.forEach(function(n) {
 		newSum = newSum + parseFloat(n);
-	})
+	});
 	return newSum;
+};
+
+module.exports.subtract = function(inputArray) {
+	var diff = inputArray[0] - inputArray[1];
+	if (inputArray[2] !== undefined) {
+		diff = 'This program can only subtract one number from another' +
+		'and will subtract the second from the first.';
+	}
+	return diff;
+};
+
+module.exports.multifly = function(inputArray) {
+	var newProd = 1;
+	inputArray.forEach(function(n) {
+		newProd = newProd * parseFloat(n);
+	});
+	return newProd;
 };
 
 
@@ -31,3 +45,5 @@ module.exports.add = function(inputArray) {
 // 	else if (oprtr = "*") {return parseInt(num1) * parseInt(num2)}
 // 	else if (oprtr = "/") {return parseInt(num1) / parseInt(num2)}
 // };
+
+
